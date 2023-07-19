0e6bd1cdc7ca7b91e1551122eb00610b3892b7e2
KejiBirdy <asuka.shigemura@benilde.edu.ph>
2023-07-19 10:09:53 +0800

    asteroids template and sprite changes

--- SymphonyOfAsteroids/Assets/Scripts/GameManager.cs
+++ SymphonyOfAsteroids/Assets/Scripts/GameManager.cs
@@ -11,13 +11,29 @@ public class GameManager : MonoBehaviour
     public BeatScroller theBS;
 
     public static GameManager instance;
+    
+    [SerializeField] List<Spawner> asteroidSpawners;
 
-    [SerializeField] Spawner A;
-    [SerializeField] Spawner J;
-    [SerializeField] Spawner S;
-    [SerializeField] Spawner K;
-    [SerializeField] Spawner D;
-    [SerializeField] Spawner L;
+    // I turned it into a list
+    
+    // [SerializeField] Spawner A;
+    // [SerializeField] Spawner J;
+    // [SerializeField] Spawner S;
+    // [SerializeField] Spawner K;
+    // [SerializeField] Spawner D;
+    // [SerializeField] Spawner L;
+
+    [System.Serializable]
+    public class AsteroidTemplate
+    {
+        public float spawnTime;
+        public int asteroidIndex; // 0 = A | 1 = S | 2 = D | J = 3 | K = 4 | L = 5
+    }
+
+    public List<AsteroidTemplate> asteroidTemplates;
+
+    private float timeSinceLastSpawn;              // Time elapsed since the last asteroid spawn
+    private int currentAsteroidTemplateIndex = 0;  // Index of the next asteroid template to use
 
     public float RUNTIME = 0;
     public float _songBpm = 60f;
@@ -26,7 +42,6 @@ public class GameManager : MonoBehaviour
     public float _songPositionInBeats;
     public float _dspSongTime;
     public float _firstBeatOffset = 1.5f;
-
 
     public int currentScore;
     public int scorePerNote = 100;
@@ -56,8 +71,10 @@ public class GameManager : MonoBehaviour
 
         track.Play();
 
-        A.SpawnAsteroid();
-        J.SpawnAsteroid();
+        StartSpawningAsteroids();
+
+        // A.SpawnAsteroid();
+        // J.SpawnAsteroid();
     }
 
     void Update()
@@ -76,9 +93,58 @@ public class GameManager : MonoBehaviour
         //L.SpawnAsteroid();
             //everything you need to call to spawn asteroids
 
-        if (RUNTIME % 5 == 0)
+        // if (RUNTIME % 5 == 0)
+        // {
+        //     K.SpawnAsteroid(); //chain ? note (discovered by accident)
+        // }
+
+        // Update timeSinceLastSpawn
+        timeSinceLastSpawn += Time.deltaTime;
+
+        // Check if it's time to spawn the next asteroid based on the current template
+        if (currentAsteroidTemplateIndex < asteroidTemplates.Count)
         {
-            K.SpawnAsteroid(); //chain ? note (discovered by accident)
+            if (timeSinceLastSpawn >= asteroidTemplates[currentAsteroidTemplateIndex].spawnTime)
+            {
+                // Spawn the asteroid using the selected spawner
+                int asteroidIndex = asteroidTemplates[currentAsteroidTemplateIndex].asteroidIndex;
+                if (asteroidIndex >= 0 && asteroidIndex < asteroidSpawners.Count)
+                {
+                    asteroidSpawners[asteroidIndex].SpawnAsteroid();
+                }
+
+                // Move to the next template
+                currentAsteroidTemplateIndex++;
+                timeSinceLastSpawn = 0f;
+            }
+        }
+    }
+
+    void StartSpawningAsteroids()
+    {
+        // Make sure the asteroid spawners list is not empty
+        if (asteroidSpawners.Count == 0)
+        {
+            Debug.LogError("No asteroid spawners assigned");
+            return;
+        }
+
+        // Reset the currentAsteroidTemplateIndex and timeSinceLastSpawn
+        currentAsteroidTemplateIndex = 0;
+        timeSinceLastSpawn = 0f;
+
+        // Sort the asteroid templates based on spawn time (in ascending order)
+        asteroidTemplates.Sort((x, y) => x.spawnTime.CompareTo(y.spawnTime));
+
+        // Start spawning asteroids based on the first template
+        if (asteroidTemplates.Count > 0)
+        {
+            if (asteroidTemplates[0].asteroidIndex >= 0 &&
+                asteroidTemplates[0].asteroidIndex < asteroidSpawners.Count)
+            {
+                asteroidSpawners[asteroidTemplates[0].asteroidIndex].SpawnAsteroid();
+            }
+            currentAsteroidTemplateIndex = 1;
         }
     }
 
